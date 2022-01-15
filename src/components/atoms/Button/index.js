627ef95618fb295647521b3b1790573a24395b62
angeljsb <suangel.007@gmail.com>
2022-01-15 12:20:02 -0400

    initial styles

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 
 const Button = (props = {}) => {
   const { color, children, className, ...otherProps } = props;
--- src/components/atoms/Title/index.js
+++ src/components/atoms/Title/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 
 const Title = (props = {}) => {
   const { size, children, className, ...otherProps } = props;
