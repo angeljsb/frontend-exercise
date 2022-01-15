dfb62b8d9552cda053ab1c90e5ec4712577436e4
angeljsb <suangel.007@gmail.com>
2022-01-15 11:55:45 -0400

    Button atom

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Button = (props = {}) => {
+  const { color, children, className, ...otherProps } = props;
+
+  const realColor = ["primary", "secondary"].includes(color)
+    ? color
+    : "primary";
+
+  const parentClass = className || "";
+
+  return (
+    <button
+      className={`button button--${realColor} ${parentClass}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
