5ffeff43beef62331137cc0a3d4b75c76ff01114
Angel Suarez <suangel.007@gmail.com>
2022-01-17 14:35:08 -0400

    Articles table

--- src/components/molecules/TableRow/index.js
+++ src/components/molecules/TableRow/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./style.css";
 
 const TableRow = (props = {}) => {
   const { register, ...otherProps } = props;
 
+  console.log(register);
+
   return (
     <tr className="table-row" {...otherProps}>
-      {register.map((value, index) => {
+      {register.map((value, index) => (
         <td className="table-row__cell" key={index}>
           {value}
-        </td>;
-      })}
+        </td>
+      ))}
     </tr>
   );
 };
--- src/components/organisms/ArticlesTable/index.js
+++ src/components/organisms/ArticlesTable/index.js
@@ -1,30 +1,53 @@
 import React from "react";
 import TableRow from "../../molecules/TableRow";
+import Container from "../../atoms/Container";
+import Link from "../../atoms/Link";
+import "./style.css";
 
 const ArticlesTable = (props = {}) => {
   const { articles, ...otherProps } = props;
 
-  const fields = ["author", "title", "content", "date", ""];
+  const formated = articles?.map((article) => {
+    return {
+      ...article,
+      date: new Date(article.date).toLocaleDateString(),
+      content:
+        article.content.length > 35
+          ? article.content.substring(0, 35) + "..."
+          : article.content,
+    };
+  });
+
+  const fields = ["author", "title", "content", "date"];
 
   return (
-    <table className="articles-table" {...otherProps}>
-      <thead>
-        <tr className="articles-table__header-row">
-          {fields.map((field, index) => (
-            <th className="articles-table__header-cell" key={index}>
-              {field}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {articles === null
-          ? ""
-          : articles.map((article, index) => (
-              <TableRow register={article} key={index}></TableRow>
+    <Container rounded className="articles-table">
+      <table className="articles-table__table" {...otherProps}>
+        <thead>
+          <tr className="articles-table__header-row">
+            {fields.map((field, index) => (
+              <th className="articles-table__header-cell" key={index}>
+                {field}
+              </th>
             ))}
-      </tbody>
-    </table>
+            <th className="articles-table__header-cell"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {articles === null ? (
+            <tr></tr>
+          ) : (
+            formated.map((article, index) => {
+              const register = fields.map((key) => article[key]);
+              register.push(
+                <Link to={`articles/${articles[index].id}`}>Edit</Link>
+              );
+              return <TableRow register={register} key={index}></TableRow>;
+            })
+          )}
+        </tbody>
+      </table>
+    </Container>
   );
 };
 
